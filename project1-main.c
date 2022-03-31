4e5bda42af48570899c452ea7c91580c7252138e
trivedimilan <85644321+trivedimilan@users.noreply.github.com>
2022-03-31 14:46:32 -0400

    Update project1-main.c

--- project1-main.c
+++ project1-main.c
@@ -6,7 +6,6 @@
 #include "stb_image_write.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize.h"
-
 #define CHANNEL_NUM 3
 
 // imatrix struct, wraps a byte array of pixel data for an image
@@ -37,69 +36,6 @@ imatrix* init_blank_rgb_image(int width, int height, int channels);
 
 // frees all allocated memory
 void free_imatrix(imatrix* image);
-
-int main(int argc,  char* argv[]) {
-    int width, height, channels;
-    int width2, height2, channels2;
-    int i;
-    uint8_t* rgb_image;
-
-    for (i=1; i<argc; ++i)
-        printf("%d:\t%s\n", i, argv[1]);
-
-    // Load an image
-    imatrix* M = init_from_file("images/test_image1.png", &width, &height, &channels);
-    if (M != NULL)
-        stbi_write_png("images/image.png", width, height, CHANNEL_NUM, M->rgb_image, width*CHANNEL_NUM);
-
-    //Load another image
-    imatrix* M2 = init_from_file("images/test_image2.png", &width2, &height2, &channels2);
-    if (M2 != NULL) 
-        stbi_write_png("images/image2.png", width2, height2, CHANNEL_NUM, M2->rgb_image, width2*CHANNEL_NUM);
-    
-
-    //resize the image to the dimensions of the first image
-    uint8_t* output_pixels = (uint8_t*)malloc(width*height*CHANNEL_NUM*sizeof(uint8_t));
-    stbir_resize_uint8(M2->rgb_image , width2 , height2 , 0,
-                        output_pixels, width, height, 0,
-                                     CHANNEL_NUM);
-    stbi_write_png("images/image3.png", width, height, CHANNEL_NUM, output_pixels, width*CHANNEL_NUM);
-    
-
-    //add two images
-    imatrix* M2_resized = init_from_rgb_image(output_pixels, width, height, channels);
-    imatrix* sum_image = M2_resized->add(M2_resized, M);
-    stbi_write_png("images/sum.png", width, height, CHANNEL_NUM, sum_image->rgb_image, width*CHANNEL_NUM);
-
-
-    //subtract them
-    imatrix* diff_image = sum_image->subtract(sum_image, M);
-    stbi_write_png("images/diff.png", width, height, CHANNEL_NUM, diff_image->rgb_image, width*CHANNEL_NUM);
-
-    //scale then add
-    float alpha = 0.4;
-    M2_resized->scale(M2_resized, width, height, alpha);
-    M->scale(M, width, height, (1-alpha));
-    imatrix* scaled_sum = M2_resized->add(M2_resized, M);
-    stbi_write_png("images/scaled_sum.png", width, height, CHANNEL_NUM, scaled_sum->rgb_image, width*CHANNEL_NUM);
-
-
-    //scaled and translate (matrix multiply)
-
-    // OPTIONAL: Test code for matrix multiply
-    
-    
-    // free memory
-    free_imatrix(M);
-    free_imatrix(M2);
-    free_imatrix(M2_resized);
-    free_imatrix(sum_image);
-    free_imatrix(diff_image);
-
-    return 0;
-}
-
-
 
 enum RGB {RED, GREEN, BLUE};
 void init_funcptrs(imatrix* this);
@@ -168,20 +104,15 @@ imatrix* init_rgb(imatrix* this, int width, int height, int channels){
     int i,j;
     this->width = width;
     this->height = height;
-
-    this->r = (uint8_t**)malloc(sizeof(uint8_t)*(height));
-    this->g = (uint8_t**)malloc(sizeof(uint8_t)*(height));
-    this->b = (uint8_t**)malloc(sizeof(uint8_t)*(height));
-
-    for(int i=0; i < height; i++){
-        this->r[i] = (uint8_t*)malloc(sizeof(uint8_t)*(width));
-        this->g[i] = (uint8_t*)malloc(sizeof(uint8_t)*(width));
-        this->b[i] = (uint8_t*)malloc(sizeof(uint8_t)*(width));
-    }
-    //allocate the space for the three channel matrices
-    
+    this->r = (uint8_t**)malloc(height * sizeof(uint8_t*));
+    this->g = (uint8_t**)malloc(height * sizeof(uint8_t*));
+    this->b = (uint8_t**)malloc(height * sizeof(uint8_t*));
+    for (i = 0; i < height; i++){
+        this->r[i] = (uint8_t*)malloc(width * sizeof(uint8_t));
+        this->g[i] = (uint8_t*)malloc(width * sizeof(uint8_t));
+        this->b[i] = (uint8_t*)malloc(width * sizeof(uint8_t));
+        }
     // YOUR CODE HERE
-
     return this;
 }
 
@@ -204,10 +135,32 @@ imatrix* set_rgb_image(imatrix* this, uint8_t* new_rgb_image, int height, int wi
 *            Note: This memory must be freed when you're done using it.
 */
 imatrix* add(imatrix* m1, imatrix* m2){
-    
-    // YOUR CODE HERE
-    
+    if (m1->height != m2->height||m1->width != m2->width)
     return NULL;
+    int height = m1->height, width = m1->width;
+    imatrix* addMatrix = init_blank_rgb_image(width, height, 3);
+        for (int i=0; i<height; i++){
+            for (int j=0; j<width; j++){
+                if((m1->r[i][j] + m2->r[i][j])>=255){
+                    addMatrix->r[i][j] = m1->r[i][j] + m2->r[i][j]-255;
+                }else
+                addMatrix->r[i][j] = m1->r[i][j] + m2->r[i][j];
+
+                if((m1->g[i][j] + m2->g[i][j])>=255){
+                    addMatrix->g[i][j] = m1->r[i][j] + m2->g[i][j]-255;
+                }else
+                addMatrix->g[i][j] = m1->g[i][j] + m2->g[i][j];
+
+                if((m1->b[i][j] + m2->b[i][j])>=255){
+                    addMatrix->b[i][j] = m1->b[i][j] + m2->b[i][j]-255;
+                }else
+                addMatrix->b[i][j] = m1->b[i][j] + m2->b[i][j];
+            }
+        }
+    addMatrix->write_rgb_to_image(addMatrix);
+    return addMatrix;
+
+    // YOUR CODE HERE
 }
 
 
@@ -223,9 +176,31 @@ imatrix* add(imatrix* m1, imatrix* m2){
 */
 imatrix* subtract(imatrix* m1, imatrix* m2){
 
-    // YOUR CODE HERE
-
+     if (m1->height != m2->height||m1->width != m2->width)
     return NULL;
+    int height = m1->height, width = m1->width;
+    imatrix* subMatrix = init_blank_rgb_image(width, height, 3);
+        for (int i=0; i<height; i++){
+            for (int j=0; j<width; j++){
+                if(m1->r[i][j] < m2->r[i][j]){
+                    subMatrix->r[i][j] = 0;
+                }else
+                subMatrix->r[i][j] = m1->r[i][j] - m2->r[i][j];
+
+                if(m1->g[i][j] < m2->g[i][j]){
+                    subMatrix->g[i][j] = 0;
+                }else
+                subMatrix->g[i][j] = m1->g[i][j] - m2->g[i][j];
+
+                if(m1->b[i][j] < m2->b[i][j]){
+                    subMatrix->b[i][j] = 0;
+                }else
+                subMatrix->b[i][j] = m1->b[i][j] - m2->b[i][j];
+            }
+        }
+    subMatrix->write_rgb_to_image(subMatrix);
+    return subMatrix;
+        // YOUR CODE HERE
 }
 
 /*
@@ -238,10 +213,36 @@ imatrix* subtract(imatrix* m1, imatrix* m2){
 *               Note: This memory must be freed after use.
 */
 imatrix* multiply(imatrix* m1, imatrix* m2){
-    
+    if (m1->height != m2->width|| m2->width < 1.0)
+        return NULL;
+        int height = m1->height, width = m2->width;
+        imatrix* res = init_blank_rgb_image(width, height, 3);
+        int i, j, k;
+        for (k=0; k<3; k++){
+            for (int i = 0; i < width; i++){
+                for (int j = 0; j < height; j++){
+                    if (k == 0){
+                        res->r[i][j] = 0;
+                    }else if(k == 1){
+                        res->g[i][j] = 0;
+                    }else{
+                        res->b[i][j] = 0;
+                    }
+                    for (int k = 0; k < height; k++){
+                        if (k == 0){
+                            res->r[i][j] += m1->r[i][k] * m2->r[k][j];
+                        }else if(k == 1){
+                            res->b[i][j] += m1->g[i][k] * m2->g[k][j];
+                        }else{
+                            res->g[i][j] += m1->b[i][k] * m2->b[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    res->write_rgb_to_image(res);
+    return res;
     // YOUR CODE HERE
- 
-    return m1;
 }
 
 /*
@@ -252,10 +253,22 @@ imatrix* multiply(imatrix* m1, imatrix* m2){
 *   @returns this with all the pixel values scaled by alpha.
 */
 imatrix* scale(imatrix* this, int width, int height, float alpha){
-    
-    // YOUR CODE HERE
+    for (int k=0; k<3; k++){
+        for (int i=0; i<height; i++){
+            for (int j=0; j<width; j++){
+                if (k == 0){
+                    this->r[i][j] *= alpha;
+                }else if(k == 1){
+                    this->g[i][j] *= alpha;
+                }else{
+                    this->b[i][j] *= alpha;
+                }
 
+            }
+        }
+    }
     return this;
+    // YOUR CODE HERE
 }
 
 void write_rgb_to_image(imatrix* m){
@@ -301,4 +314,67 @@ void free_imatrix(imatrix* image_matrix){
     free(image_matrix->b);
 
     stbi_image_free(image_matrix->rgb_image);
+}
+
+int main(int argc,  char* argv[]) {
+    int width, height, channels;
+    int width2, height2, channels2;
+    int i;
+    uint8_t* rgb_image;
+    
+    for (i=1; i<argc; ++i)
+        printf("%d:\t%s\n", i, argv[1]);
+
+    // Load an image
+    imatrix* M = init_from_file("images/red.png", &width, &height, &channels);
+    if (M != NULL)
+        stbi_write_png("images/image.png", width, height, CHANNEL_NUM, M->rgb_image, width*CHANNEL_NUM);
+
+    //Load another image
+    imatrix* M2 = init_from_file("images/blue.png", &width2, &height2, &channels2);
+    if (M2 != NULL) 
+        stbi_write_png("images/image2.png", width2, height2, CHANNEL_NUM, M2->rgb_image, width2*CHANNEL_NUM);
+    
+
+    //resize the image to the dimensions of the first image
+    uint8_t* output_pixels = (uint8_t*)malloc(width*height*CHANNEL_NUM*sizeof(uint8_t));
+    stbir_resize_uint8(M2->rgb_image , width2 , height2 , 0,
+                        output_pixels, width, height, 0,
+                                     CHANNEL_NUM);
+    stbi_write_png("images/image3.png", width, height, CHANNEL_NUM, output_pixels, width*CHANNEL_NUM);
+    
+
+    //add two images
+    imatrix* M2_resized = init_from_rgb_image(output_pixels, width, height, channels);
+    imatrix* sum_image = M2_resized->add(M2_resized, M);
+    stbi_write_png("images/sum.png", width, height, CHANNEL_NUM, sum_image->rgb_image, width*CHANNEL_NUM);
+
+
+    //subtract them
+    imatrix* diff_image = sum_image->subtract(sum_image, M);
+    stbi_write_png("images/diff.png", width, height, CHANNEL_NUM, diff_image->rgb_image, width*CHANNEL_NUM);
+
+    //scale then add
+    float alpha = 0.4;
+    M2_resized->scale(M2_resized, width, height, alpha);
+    M->scale(M, width, height, (1-alpha));
+    imatrix* scaled_sum = M2_resized->add(M2_resized, M);
+    stbi_write_png("images/scaled_sum.png", width, height, CHANNEL_NUM, scaled_sum->rgb_image, width*CHANNEL_NUM);
+
+
+    //scaled and translate (matrix multiply)
+    imatrix* res_image = M2_resized->multiply(M2_resized, M);
+    stbi_write_png("images/multiply.png", width, height, CHANNEL_NUM, res_image->rgb_image, width*CHANNEL_NUM);
+
+    // OPTIONAL: Test code for matrix multiply
+    
+    
+    // free memory
+    free_imatrix(M);
+    free_imatrix(M2);
+    free_imatrix(M2_resized);
+    free_imatrix(sum_image);
+    free_imatrix(diff_image);
+
+    return 0;
 }
